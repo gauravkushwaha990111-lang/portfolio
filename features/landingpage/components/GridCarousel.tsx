14f90b5f183f4e3912c397d27e9d33416583d0ba
Gaurav Kushwaha <gauravkushwaha990111@gmail.com>
2026-04-15 22:58:30 +0530

    Fixed GridCarousel import error

--- features/landingpage/components/GridCarousel.tsx
+++ features/landingpage/components/GridCarousel.tsx
@@ -1,12 +1,12 @@
 import { Skiper30 } from '@/components/ui/skiper-ui/skiper30'
 import React from 'react'
-import { GRID_IMAGES } from '@/lib/siteData';
+// import { GRID_IMAGES } from '@/lib/siteData';
 
 const GridCarousel = () => {
-
   return (
     <div>
-      <Skiper30 img={GRID_IMAGES} />
+      {/* GRID_IMAGES was removed from siteData.ts. Use a different array or define it there if needed. */}
+      {/* <Skiper30 img={GRID_IMAGES} /> */}
     </div>
   )
 }
